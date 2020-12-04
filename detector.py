e63fd668028bffa609064457be166ae6b5cced7d
nicolascuiza <robertocuiza@itibb.edu.bo>
2020-12-04 11:10:48 -0400

    version 1

---
+++ detector.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 01:32:49 2020
+
+@author: Display
+"""
+
+import cv2
+import numpy as np
+faceClassif= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+image= cv2.imread('oficina.png')
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+faces=faceClassif.detectMultiScale(gray,
+                                   scaleFactor=1.1,
+                                   minNeighbors=5,
+                                   minSize=(30,30),
+                                   maxSize=(200,200))
+for(x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+    
+    
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
